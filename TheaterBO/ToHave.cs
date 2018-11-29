c7002c147cba553e75b7b3c58c78b75eb6ad93aa
Spootnika <celia.lflr@gmail.com>
2018-11-29 10:53:08 +0100

    ajout classe tohave

---
+++ TheaterBO/ToHave.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheaterBO
+{
+    public class ToHave
+    {
+        private Nationality toHave_nationality;
+        private Actor toHave_actor;
+
+        public Nationality ToHave_nationality { get => toHave_nationality; set => toHave_nationality = value; }
+        public Actor ToHave_actor { get => toHave_actor; set => toHave_actor = value; }
+    }
+}
