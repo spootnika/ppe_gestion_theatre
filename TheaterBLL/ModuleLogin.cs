be6da839a9c90510359e6425e437fa8760ccd537
Spootnika <celia.lflr@gmail.com>
2018-12-01 12:00:07 +0100

    Commentaires BLL

--- TheaterBLL/ModuleLogin.cs
+++ TheaterBLL/ModuleLogin.cs
@@ -8,5 +8,17 @@ namespace TheaterBLL
 {
     public class ModuleLogin
     {
+
+        // objet BLL
+
+        // Accesseur en lecture
+
+        // Définit la chaîne de connexion grâce à la méthode SetchaineConnexion de la DAL 
+
+        // Méthode qui renvoit un objet User correspondant au pseudo passé en paramètre
+        // -> GetUtilisateur
+
+        // Méthode qui créer un objet de LoginInfo avec les attributs de l'objet User passé en paramètre
+
     }
 }
