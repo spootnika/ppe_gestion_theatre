f78d055cca86c63f3e7b7891182c5a71880581c0
Valantina80 <a.f.@hotmail.fr>
2018-12-01 16:37:56 +0100

    Couche DAL module authentification

--- TheaterDAL/LoginDAO.cs
+++ TheaterDAL/LoginDAO.cs
@@ -3,18 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using TheaterBO;
 
 namespace TheaterDAL
 {
     public class LoginDAO
     {
-
-        // objet DAO
+        private static LoginDAO unUtilisateurDAO;
 
         // Accesseur en lecture, renvoi une instance
+        public static LoginDAO GetUser()
+        {
+            if (unUtilisateurDAO == null)
+            {
+                unUtilisateurDAO = new LoginDAO();
+            }
+            return unUtilisateurDAO;
+        }
 
-        // Méthode qui renvoie un objet user correspondant au pseudo passé en paramètre
-        // -> GetUser
 
-    }
-}
+        //retourne un utilisateur en fonction de son pseudo
+        public static LoginDAO GetUser(string pseudo)
+        {
+            int id;
+            string password;
+            bool isAdmin;
+            // Connexion à la BD
+            SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = maConnexion;
+            cmd.CommandText = " SELECT * FROM AppUser WHERE user_pseudo == "+pseudo;
+
+            SqlDataReader monReader = cmd.ExecuteReader();
+            while (monReader.Read())
+            {
+                id = Int32.Parse(monReader["user_id"].ToString());
+                password = monReader["user_password"].ToString();
+                isAdmin = (bool)monReader["user_isAdmin"];
+                unUtilisateurDAO = new AppUser(id, pseudo, password, isAdmin);
+
+            }
+
+                return unUtilisateurDAO;
+        }
+       }
+   }
