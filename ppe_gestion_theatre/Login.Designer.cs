cfb87552509c0134bc2276eba84efeed369356cb
Spootnika <celia.lflr@gmail.com>
2018-12-01 09:12:46 +0100

    Maj form login

--- ppe_gestion_theatre/Login.Designer.cs
+++ ppe_gestion_theatre/Login.Designer.cs
@@ -30,35 +30,91 @@
         {
             this.lblLogin = new System.Windows.Forms.Label();
             this.lblPassword = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblConnect = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblLogin
             // 
+            this.lblLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblLogin.AutoSize = true;
-            this.lblLogin.Location = new System.Drawing.Point(132, 87);
+            this.lblLogin.Location = new System.Drawing.Point(92, 85);
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(42, 13);
+            this.lblLogin.Size = new System.Drawing.Size(49, 13);
             this.lblLogin.TabIndex = 0;
-            this.lblLogin.Text = "Login : ";
+            this.lblLogin.Text = "Pseudo :";
             this.lblLogin.Click += new System.EventHandler(this.label1_Click);
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(97, 114);
+            this.lblPassword.Location = new System.Drawing.Point(64, 112);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(77, 13);
             this.lblPassword.TabIndex = 1;
             this.lblPassword.Text = "Mot de passe :";
             // 
-            // Form1
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(147, 82);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(147, 109);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(117, 175);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Valider";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblConnect
+            // 
+            this.lblConnect.AutoSize = true;
+            this.lblConnect.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConnect.Location = new System.Drawing.Point(12, 9);
+            this.lblConnect.Name = "lblConnect";
+            this.lblConnect.Size = new System.Drawing.Size(114, 29);
+            this.lblConnect.TabIndex = 5;
+            this.lblConnect.Text = "Connexion";
+            // 
+            // lblError
+            // 
+            this.lblError.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblError.Location = new System.Drawing.Point(67, 142);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(180, 23);
+            this.lblError.TabIndex = 6;
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 310);
+            this.ClientSize = new System.Drawing.Size(331, 222);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblConnect);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLogin);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblLogin);
-            this.Name = "Form1";
+            this.Name = "Login";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -69,6 +125,11 @@
 
         private System.Windows.Forms.Label lblLogin;
         private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblConnect;
+        private System.Windows.Forms.Label lblError;
     }
 }
 
