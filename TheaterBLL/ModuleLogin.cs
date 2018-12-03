8695bd63db0788a671dbad82657801f6572c7abc
Guigs95 <dominici.guillaume@gmail.com>
2018-12-03 20:32:47 +0100

    ModuleLogin - TheaterBLL - OK

--- TheaterBLL/ModuleLogin.cs
+++ TheaterBLL/ModuleLogin.cs
@@ -3,30 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TheaterBO;
+using System.Configuration;
+using TheaterBO; // Réference la couche BO
+using TheaterDAL; // Réference la couche DAL
 
 namespace TheaterBLL
 {
     public class ModuleLogin
     {
-
-        // objet BLL
-        private static ModuleLogin moduleLogin;
-
+        
+        private static ModuleLogin moduleLogin; // objet BLL
+        
         // Accesseur en lecture
+        public static ModuleLogin GetModuleLogin()
+        {
+            if (moduleLogin == null)
+            {
+                moduleLogin = new ModuleLogin();
+            }
+            return moduleLogin;
+        }
+
+
 
         // Définit la chaîne de connexion grâce à la méthode SetchaineConnexion de la DAL 
+        public static void SetchaineConnexion(ConnectionStringSettings chset)
+        {
+            string chaine = chset.ConnectionString;
+            ConnexionBD.GetConnexionBD().SetchaineConnexion(chaine);
+        }
 
         // Méthode qui renvoit un objet User correspondant au pseudo passé en paramètre
         public static AppUser GetUser(string pseudo)
         {
-
+            return LoginDAO.GetUser(pseudo);
         }
 
         // Méthode qui créer ET RENVOIE un objet de LoginInfo avec les attributs de l'objet User passé en paramètre
         public static LoginInfo CreateLoginInfo(AppUser currentUser)
         {
+            LoginInfo login;
+            login = new LoginInfo(currentUser);
 
+            return login;
         }
 
     }
