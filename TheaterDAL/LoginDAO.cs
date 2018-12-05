38d0a07e2fb38bc2d44030a20679779c79b86ef7
Spootnika <celia.lflr@gmail.com>
2018-12-05 11:47:36 +0100

    corrections dao

--- TheaterDAL/LoginDAO.cs
+++ TheaterDAL/LoginDAO.cs
@@ -26,27 +26,48 @@ namespace TheaterDAL
         //retourne un utilisateur en fonction de son pseudo
         public static AppUser GetUser(string pseudo)
         {
-            int id;
-            string password;
-            bool isAdmin;
-     
+            // définition des variables
+            AppUser unUtilisateur;
+            int id = 0;
+            string password = "";
+            bool isAdmin = false;
+            SqlConnection maConnexion;
+            
             // Connexion à la BD
-            SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+            maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+           
+            // initialisationn d'un reader
+            SqlDataReader monReader = null;
+            
+            // initialisation et écriture d'une requête sql
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = maConnexion;
             cmd.CommandText = "SELECT * FROM AppUser WHERE user_pseudo = '" + pseudo + "'";
 
-            SqlDataReader monReader = cmd.ExecuteReader();
-         
-            id = Int32.Parse(monReader["user_id"].ToString());
-            password = monReader["user_password"].ToString();
-            isAdmin = (bool)monReader["user_isAdmin"];
-            AppUser unUtilisateur = new AppUser(id, pseudo, password, isAdmin);
+            // execution du reader
+            monReader = cmd.ExecuteReader();
 
-            
+            // vérif si retourne quelque chose ou non
+            if (monReader.HasRows)
+            {
+                // while reader
+
+                while (monReader.Read())
+                {
+                    id = Convert.ToInt32(monReader["user_id"].ToString());
+                    password = monReader["user_password"].ToString();
+                    isAdmin = (bool)monReader["user_isAdmin"];
+                }
+            }
+
+            unUtilisateur = new AppUser(id, pseudo, password, isAdmin);
+            monReader.Close();
+
             // Fermeture de la connexion
             maConnexion.Close();
+
             return unUtilisateur;
+
         }
-       }
-   }
+    }
+}
