88796f89bd673498fd1c0beed361a8a42312f569
Spootnika <celia.lflr@gmail.com>
2019-03-28 10:42:56 +0100

    load dgv synthèse

--- ppe_gestion_theatre/Synthese.cs
+++ ppe_gestion_theatre/Synthese.cs
@@ -39,7 +39,50 @@ namespace ppe_gestion_theatre
         // Chargement de la liste en fonctione des dates pour filtrer
         private void LoadDataGridView(DateTime dateDeb, DateTime dateFin)
         {
+            List<TheaterPiece> lesPieces = ModuleSynthese.GetTheaterPieces();
 
+            DataTable dt = new DataTable();
+            dgvListeSynthese.DataSource = dt;
+
+            dt.Columns.Add(new DataColumn("piece", typeof(string)));
+            dgvListeSynthese.Columns["piece"].HeaderText = "Pièce";
+
+            dt.Columns.Add(new DataColumn("nbRepres", typeof(int)));
+            dgvListeSynthese.Columns["nbRepres"].HeaderText = "Nombre représentations";
+
+            dt.Columns.Add(new DataColumn("nbSpecTotal", typeof(int)));
+            dgvListeSynthese.Columns["nbSpecTotal"].HeaderText = "Nombre total de spectateurs";
+
+            dt.Columns.Add(new DataColumn("nbSpecMoyen", typeof(int)));
+            dgvListeSynthese.Columns["nbSpecMoyen"].HeaderText = "Nombre moyen de spectateurs";
+
+            dt.Columns.Add(new DataColumn("caRealise", typeof(float)));
+            dgvListeSynthese.Columns["caRealise"].HeaderText = "CA réalisé";
+
+            dt.Columns.Add(new DataColumn("caMoyenRealise", typeof(float)));
+            dgvListeSynthese.Columns["caMoyenRealise"].HeaderText = "CA moyen réalisé";
+
+            dgvListeSynthese.ReadOnly = true;
+
+
+            //test dgv
+            foreach (TheaterPiece unePiece in lesPieces)
+            {
+                string nomPiece = unePiece.TheaterPiece_name;
+
+                int nbRepres = ModuleSynthese.GetNbShows(dateDeb, dateFin);
+
+                int nbSpecTotal = ModuleSynthese.GetNbSpectators(unePiece.TheaterPiece_id, dateDeb, dateFin);
+
+                int nbSpecMoyen = nbSpecTotal / nbRepres;
+
+                float caRealise = ModuleSynthese.GetCaTotal(unePiece.TheaterPiece_id, dateDeb, dateFin);
+
+                float caMoyenRealise = caRealise / nbRepres;
+                
+
+                dt.Rows.Add(nomPiece, nbRepres, nbSpecTotal, nbSpecMoyen, caRealise, caMoyenRealise);
+            }
         }
     }
 }
